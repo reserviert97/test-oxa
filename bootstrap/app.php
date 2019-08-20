fe9115b57d01490fedcfb49478756ad5bd7965f0
reserviert97 <nurlatif97@outlook.com>
2019-08-20 16:08:15 +0700

    add table user migration and seeder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,13 +57,13 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\ExampleMiddleware::class
+]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -77,8 +77,8 @@ $app->singleton(
 */
 
 $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,8 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'name'     => $faker->name,
+        'email'    => $faker->unique()->email,
+        'password' => password_hash('123456', PASSWORD_BCRYPT),
     ];
 });
